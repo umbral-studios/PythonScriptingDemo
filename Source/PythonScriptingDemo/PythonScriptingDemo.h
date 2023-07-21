26600a1ad52a76221f73a9fe9af803e6f32473b4
Leonard Parisi <email@leonardparisi.com>
2023-07-21 10:13:26 -0400

    Removed reference material in preparation for tutorial

--- Source/PythonScriptingDemo/PythonScriptingDemo.h
+++ Source/PythonScriptingDemo/PythonScriptingDemo.h
@@ -1,13 +1,3 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
-
-UENUM(BlueprintType)
-enum ERarity
-{
-	Common UMETA(DisplayName = "Common"),
-	Uncommon UMETA(DisplayName = "Uncommon"),
-	Rare UMETA(DisplayName = "Rare"),
-	Legendary UMETA(DisplayName = "Legendary"),
-};
